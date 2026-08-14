b131e0356648530871605bc591be69dfa4de7825
Shubhasmita <127921000+shubhasmita2o1@users.noreply.github.com>
2026-08-14 12:14:29 +0530

    Import nav-dropdown CSS for Services menu UX

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -11,6 +11,7 @@ import { useEffect, type ReactNode } from "react";
 
 import appCss from "../styles.css?url";
 import "../styles.css";
+import "../styles/nav-dropdown.css";
 import { reportLovableError } from "../lib/lovable-error-reporting";
 import { useDocumentDirection } from "@/hooks/use-document-direction";
 
